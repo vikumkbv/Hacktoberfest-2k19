17ed16683a3f668bc71cf17de6bac0210a5ba609
Piyush Karira <piyushkarira1996@gmail.com>
2019-10-03 00:35:27 +0530

    Merge 2 sorted arrays

---
+++ C++/Merge_Sorting.cpp
@@ -0,0 +1,56 @@
+/*Given two sorted arrays arr1[] and arr2[] in non-decreasing order with size n and m. The task is to merge the two sorted arrays into one sorted array (in non-decreasing order).*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    long a[x+1],b[y+1];
+	    for(int i=0;i<x;i++)
+	    cin>>a[i];
+	    for(int i=0;i<y;i++)
+	    cin>>b[i];
+	    
+	    int i=0,j=0;
+	    vector<long> vec;
+	    while(i<x&&j<y)
+	    {
+	        if(a[i]<b[j])
+	        {
+	            vec.push_back(a[i]);
+	            i++;
+	        }
+	        else if(a[i]>b[j])
+	        {
+	            vec.push_back(b[j]);
+	            j++;
+	        }
+	        else if(a[i]==b[j])
+	        {
+	            vec.push_back(a[i]);
+	            vec.push_back(b[j]);
+	            i++;
+	            j++;
+	        }
+        }
+        while(i<x)
+        {
+            vec.push_back(a[i]);
+            i++;
+        }
+        while(j<y){
+            vec.push_back(b[j]);
+            j++;
+        }
+        for(int i=0;i<vec.size();i++)
+        cout<<vec[i]<<" ";
+        
+        cout<<endl;
+	}
+	return 0;
+}
