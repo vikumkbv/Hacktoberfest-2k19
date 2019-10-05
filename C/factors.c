67db4b5d8836a6e9fab4b828ca35f8a25446127e
naveenreddy2000 <konugantinaveenreddy@gmail.com>
2019-10-05 13:03:02 +0530

    some c-programs are added

---
+++ C/factors.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int number, i;
+    printf("Enter a positive integer: ");
+    scanf("%d",&number);
+    printf("Factors of %d are: ", number);
+    for(i=1; i <= number; ++i)
+    {
+        if (number%i == 0)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
