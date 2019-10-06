54b037a9b627e41d48c428061e55bc7cbb48ced8
DishitaBadarshahi <dishitabadarshahi@gmail.com>
2019-10-06 20:49:18 +0530

    Create random.java

---
+++ java/random.java
@@ -0,0 +1,11 @@
+import java.util.*; 
+  
+public class generateRandom 
+{ 
+    public static void main(String args[]) 
+    { 
+        // Generating random doubles 
+        System.out.println("Random doubles: " + Math.random()); 
+        
+    } 
+} 
