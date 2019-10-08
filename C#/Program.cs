7769fcb629f1f07d44932962ddb7179d77ae50f7
celso4364anjos <gabrieldias2032@gmail.com>
2019-10-08 15:07:48 -0300

    caesar cipher c#

---
+++ C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = 0;
+            Console.WriteLine("choose the key of cipher: ");
+            int key = int.Parse(Console.ReadLine());
+            Console.WriteLine("type the message: ");
+            string input = Console.ReadLine();
+            char[] result = input.ToCharArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var = result[i];
+                result[i] = result[i] == ' ' ? ' ' : (char)(var + key);
+
+            }
+            input = new string(result);
+            Console.WriteLine(input);
+        }
+    }
+}
