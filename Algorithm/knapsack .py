4f026e10cd6bafc2ce9722b727e0b7c264505514
Sumit Jain <42279031+sumitjain0695@users.noreply.github.com>
2019-10-15 03:19:17 +0530

    Update knapsack .py

--- Algorithm/knapsack .py
+++ Algorithm/knapsack .py
@@ -19,3 +19,22 @@ def create_knapsack(item_count=5):
     x_capacity = np.random.randint(1, 99)
     y = brute_force_knapsack(x_weights, x_prices, x_capacity)
     return x_weights, x_prices, x_capacity, y
+
+class Item(object):
+
+    def __init__(self, value, weight):
+        self.value = value
+        self.weight = weight
+
+
+def get_maximum_value(items, capacity):
+    dp = [0] * (capacity + 1)
+    for item in items:
+        dp_tmp = [total_value for total_value in dp]
+        for current_weight in range(capacity + 1):
+            total_weight = current_weight + item.weight
+            if total_weight <= capacity:
+                dp_tmp[total_weight] = max(dp_tmp[total_weight],
+                                           dp[current_weight] + item.value)
+        dp = dp_tmp
+    return max(dp)
