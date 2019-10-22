6418844a391bffc9b64f9b2daf00826ef03c1f9c
omigus <33053642+omigus@users.noreply.github.com>
2019-10-22 11:30:38 +0700

    Create Pyramid.java

---
+++ java/Pyramid.java
@@ -0,0 +1,28 @@
+import java.io.*; 
+
+public class Pyramid 
+{ 
+    public static void makePyramid(int n) 
+    { 
+        for (int i=0; i<n; i++) 
+        { 
+
+            for (int j=n-i; j>1; j--) 
+            { 
+                System.out.print(" "); 
+            } 
+
+            for (int j=0; j<=i; j++ ) 
+            { 
+                System.out.print("* "); 
+            } 
+            System.out.println(); 
+        } 
+    } 
+      
+    public static void main(String args[]) 
+    { 
+        int n = 5; 
+        makePyramid(n); 
+    } 
+} 
