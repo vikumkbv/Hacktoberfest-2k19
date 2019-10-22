193a4b3246a17ff6de3fed9ed0e4fb5d6b36c54e
Panudet Panumas <47716860+panudet-24mb@users.noreply.github.com>
2019-10-22 11:35:18 +0700

    Create sum.php
    Sum 2 value

---
+++ php/sum.php
@@ -0,0 +1,17 @@
+<?php
+
+function welcome() {
+    echo "Welcome to Mycode.\n";
+}
+
+function hello($name) {
+    echo "Hello $name!\n";
+}
+
+function add_number($a, $b) {
+    $sum = $a + $b;
+    echo "$a + $b = $sum\n";
+}
+
+
+?>
