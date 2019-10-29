02e2477e0c4a394b564e47ba43d4449bbd323a48
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2019-10-29 13:00:37 +0800

    Create while loop in factorial program to keep querying for factorials

--- python/factorial.py
+++ python/factorial.py
@@ -1,16 +1,33 @@
 #Program to find factorial of a user inputed number
 #Eg : factorial of 5 = 5*2*3*2*1 , factorial of 7 = 7*6*5*4*3*2*1
 
-num = int(input("Enter a number: "))
+factorials = {0:1}
 
-factorial = 1
+def findFactorialOf(num):
+    
+    # check if the number is negative, positive or zero
+    if num == 0 or num == 1:
+        return 1
+    else:
+        if num in factorials.keys():
+            return factorials[num]
+        else:
+            print("Factorial of {} not found. Adding to factorials dictionary...".format(num))
+            factorials[num] = findFactorialOf(num - 1) * num
+            return factorials[num]
 
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Negative numbers don't have factorial!")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+
+
+userInput = input("Enter a number or (q)uit: ")
+
+while userInput != "q":
+    factorialNum = int(userInput)
+    if factorialNum < 0:
+        print("Negative numbers don't have factorial!")
+    else:
+        print("The factorial of {} is {}".format(factorialNum, findFactorialOf(factorialNum)))
+    userInput = input("Enter a number or (q)uit: ")
+
+
+
+
