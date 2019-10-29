263e6141a173ea087c716f5224149315e5472981
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2019-10-29 13:31:10 +0800

    Refactor fibonacci program
    - Implemented fibonacci method to calculate new and/or lookup old fibonacci queries
    - Implement fibonacciNums dictionary to store previously calculated fibonacci queries

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,10 +1,14 @@
-def fibonacci(n):
-	if(n == 0 or n == 1):
-		return n
-	return n+fibonacci(n-1)
+fibonacciNums = {0:0, 1:1}
 
-x = int(input("Enter a positive number, or quit by entering a negative number"))
+def fibonacci(n):
+    if n in fibonacciNums.keys():
+        return fibonacciNums[n]
+    else:
+        fibonacciNums[n] = fibonacci(n-1) + fibonacci(n-2)
+        return fibonacciNums[n]
+
+x = int(input("Enter a positive number, or quit by entering a negative number: "))
 
 while x >= 0:
-    print(fibonacci(x))
-    x = int(input())
+    print("{}th fibonacci is {}".format(x, fibonacci(x)))
+    x = int(input("Enter a positive number, or quit by entering a negative number: "))
