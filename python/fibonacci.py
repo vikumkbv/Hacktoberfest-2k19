73c69c25f593a457b5a24606745b3a1c828a6216
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2019-10-29 13:19:59 +0800

    Implement while loop to keep querying for fibonacci inputs in fibonacci program

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,7 +1,10 @@
-def fibonaachi(n):
+def fibonacci(n):
 	if(n == 0 or n == 1):
 		return n
-	return n+fibonaachi(n-1)
+	return n+fibonacci(n-1)
 
-x = int(input())
-print(fibonaachi(x))
+x = int(input("Enter a positive number, or quit by entering a negative number"))
+
+while x >= 0:
+    print(fibonacci(x))
+    x = int(input())
