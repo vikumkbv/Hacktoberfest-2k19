a40c7e1c92901f8a9d373cf3391a127db2cdaa99
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2019-10-29 13:04:48 +0800

    Add factorials table and factorial calculator function to speed up query in factorial program

--- python/factorial.py
+++ python/factorial.py
@@ -1,33 +1,20 @@
 #Program to find factorial of a user inputed number
 #Eg : factorial of 5 = 5*2*3*2*1 , factorial of 7 = 7*6*5*4*3*2*1
 
-factorials = {0:1}
-
-def findFactorialOf(num):
-    
-    # check if the number is negative, positive or zero
-    if num == 0 or num == 1:
-        return 1
-    else:
-        if num in factorials.keys():
-            return factorials[num]
-        else:
-            print("Factorial of {} not found. Adding to factorials dictionary...".format(num))
-            factorials[num] = findFactorialOf(num - 1) * num
-            return factorials[num]
-
-
-
 userInput = input("Enter a number or (q)uit: ")
 
+factorial = 1
+
 while userInput != "q":
-    factorialNum = int(userInput)
-    if factorialNum < 0:
-        print("Negative numbers don't have factorial!")
+
+    num = int(userInput)
+    # check if the number is negative, positive or zero
+    if num < 0:
+       print("Negative numbers don't have factorial!")
+    elif num == 0:
+       print("The factorial of 0 is 1")
     else:
-        print("The factorial of {} is {}".format(factorialNum, findFactorialOf(factorialNum)))
+       for i in range(1,num + 1):
+           factorial = factorial*i
+       print("The factorial of", num, "is", factorial)
     userInput = input("Enter a number or (q)uit: ")
-
-
-
-
